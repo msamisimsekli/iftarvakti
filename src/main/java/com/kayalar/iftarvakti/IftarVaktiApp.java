a984965ea77eb0d703e1be7b1bdbf92a9484b7d9
sami.simsekli <sami.simsekli@lnsamisimsekli.uzay.tubitak.gov.tr>
2020-04-25 21:20:17 +0300

    removed unused imports

--- src/main/java/com/kayalar/iftarvakti/IftarVaktiApp.java
+++ src/main/java/com/kayalar/iftarvakti/IftarVaktiApp.java
@@ -1,18 +1,11 @@
 package com.kayalar.iftarvakti;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-import org.apache.http.HttpException;
-import org.apache.http.client.ClientProtocolException;
-
 import com.kayalar.iftarvakti.model.RemainingTime;
 import com.kayalar.iftarvakti.service.IftarVaktiService;
 
 public class IftarVaktiApp {
 
-	public static void main(String[] args)
-			throws NumberFormatException, ClientProtocolException, IOException, URISyntaxException, HttpException {
+	public static void main(String[] args) {
 		IftarVaktiService service = new IftarVaktiService();
 
 		RemainingTime rt = service.askForCity("ISTANBUL");
