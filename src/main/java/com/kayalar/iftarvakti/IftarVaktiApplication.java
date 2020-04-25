6ce3f852e6456f24e09085669c99bbe0ecef6e4e
sami.simsekli <sami.simsekli@lnsamisimsekli.uzay.tubitak.gov.tr>
2020-04-25 13:48:25 +0300

    Initial spring boot app

---
+++ src/main/java/com/kayalar/iftarvakti/IftarVaktiApplication.java
@@ -0,0 +1,12 @@
+package com.kayalar.iftarvakti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class IftarVaktiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IftarVaktiApplication.class, args);
+	}
+}
