aa05d3208b0ff999cbc5bb5d38c90bc99ff7412e
Sinan Mutlu <sinanmutlu@alumni.bilkent.edu.tr>
2020-04-26 01:19:08 +0300

    Telegram Bot and configurations added

--- src/main/java/com/kayalar/iftarvakti/IftarVaktiApp.java
+++ src/main/java/com/kayalar/iftarvakti/IftarVaktiApp.java
@@ -1,14 +1,31 @@
 package com.kayalar.iftarvakti;
 
 import com.kayalar.iftarvakti.service.IftarVaktiService;
+import com.kayalar.iftarvakti.telegram.IftarVaktiBot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class IftarVaktiApp {
 
 	public static void main(String[] args) {
 		IftarVaktiService service = new IftarVaktiService();
 
+		ApiContextInitializer.init();
+
+		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+
+		try {
+			telegramBotsApi.registerBot(new IftarVaktiBot());
+
+		} catch (
+				TelegramApiException e) {
+			e.printStackTrace();
+		}
+
 		String rt = service.askForCity("İSTANBUL");
 
 		System.out.println(rt);
 	}
+
 }
