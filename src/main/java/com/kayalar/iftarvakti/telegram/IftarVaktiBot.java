bf8db685061e3cc20b6b74d80557cf6e4d6fb81e
Sami <msami.simsekli@gmail.com>
2020-04-26 11:44:10 +0300

    turkish chars correction

--- src/main/java/com/kayalar/iftarvakti/telegram/IftarVaktiBot.java
+++ src/main/java/com/kayalar/iftarvakti/telegram/IftarVaktiBot.java
@@ -11,85 +11,91 @@ import java.util.List;
 
 public class IftarVaktiBot extends TelegramLongPollingBot {
 
-    private List<String> cities = Arrays.asList(new String[]{"ADANA","ADIYAMAN","AFYON","AGRI","AMASYA","ANKARA","ANTALYA","ARTVIN","AYDIN","BALIKESIR","BILECIK","BINGOL","BITLIS","BOLU","BURDUR","BURSA","CANAKKALE","CANKIRI","CORUM","DENIZLI","DIYARBAKIR","EDIRNE","ELAZIG","ERZINCAN","ERZURUM","ESKISEHIR","GAZIANTEP","GIRESUN","GUMUSHANE","HAKKARI","HATAY","ISPARTA","MERSIN","ISTANBUL","IZMIR","KARS","KASTAMONU","KAYSERI","KIRKLARELI","KIRSEHIR","KOCAELI","KONYA","KUTAHYA","MALATYA","MANISA","KAHRAMANMARAS","MARDIN","MUGLA","MUS","NEVSEHIR","NIGDE","ORDU","RIZE","SAKARYA","SAMSUN","SIIRT","SINOP","SIVAS","TEKIRDAG","TOKAT","TRABZON","TUNCELI","SANLIURFA","USAK","VAN","YOZGAT","ZONGULDAK","AKSARAY","BAYBURT","KARAMAN","KIRIKKALE","BATMAN","SIRNAK","BARTIN","ARDAHAN","IGDIR","YALOVA","KARABUK","KILIS","OSMANIYE","DUZCE"});
-    IftarVaktiService service = new IftarVaktiService();
+	private List<String> cities = Arrays.asList(new String[] { "ADANA", "ADIYAMAN", "AFYON", "AĞRI", "AMASYA", "ANKARA",
+			"ANTALYA", "ARTVİN", "AYDIN", "BALIKESİR", "BİLECİK", "BİNGOL", "BİTLİS", "BOLU", "BURDUR", "BURSA",
+			"ÇANAKKALE", "ÇANKIRI", "ÇORUM", "DENİZLİ", "DİYARBAKIR", "EDİRNE", "ELAZIĞ", "ERZİNCAN", "ERZURUM",
+			"ESKİSEHİR", "GAZİANTEP", "GİRESUN", "GÜMÜŞHANE", "HAKKARİ", "HATAY", "ISPARTA", "MERSİN", "İSTANBUL",
+			"İZMİR", "KARS", "KASTAMONU", "KAYSERİ", "KIRKLARELİ", "KIRŞEHİR", "KOCAELİ", "KONYA", "KÜTAHYA", "MALATYA",
+			"MANİSA", "KAHRAMANMARAŞ", "MARDİN", "MUĞLA", "MUŞ", "NEVŞEHİR", "NİĞDE", "ORDU", "RİZE", "SAKARYA",
+			"SAMSUN", "SİİRT", "SİNOP", "SİVAS", "TEKİRDAĞ", "TOKAT", "TRABZON", "TUNCELİ", "ŞANLIURFA", "UŞAK", "VAN",
+			"YOZGAT", "ZONGULDAK", "AKSARAY", "BAYBURT", "KARAMAN", "KIRIKKALE", "BATMAN", "ŞIRNAK", "BARTIN",
+			"ARDAHAN", "IĞDIR", "YALOVA", "KARABÜK", "KİLİS", "OSMANİYE", "DÜZCE" });
+	IftarVaktiService service = new IftarVaktiService();
 
-    public void onUpdateReceived(Update update) {
+	public void onUpdateReceived(Update update) {
 
-        //System.out.println(update.getMessage().getText());
-        //System.out.println(update.getMessage().getDate());
-        //System.out.println(update.getMessage().getFrom());
+		// System.out.println(update.getMessage().getText());
+		// System.out.println(update.getMessage().getDate());
+		// System.out.println(update.getMessage().getFrom());
 
-        String command = update.getMessage().getText();
+		String command = update.getMessage().getText();
 
-        SendMessage message = new SendMessage();
+		SendMessage message = new SendMessage();
 
-        System.out.println(update.getMessage().getFrom().getUserName() + " : " + command);
+		System.out.println(update.getMessage().getFrom().getUserName() + " : " + command);
 
-        //TODO: Commandlist db conf olup cache'de tutulacak
-        if(command.equals("/start")){
+		// TODO: Commandlist db conf olup cache'de tutulacak
+		if (command.equals("/start")) {
 
-            message.setText("Hoşgeldiniz! /help yazarak bot hakkında bilgi alabilirsiniz.");
-        }
+			message.setText("Hoşgeldiniz! /help yazarak bot hakkında bilgi alabilirsiniz.");
+		}
 
-        else if (command.equals("/help")){
+		else if (command.equals("/help")) {
 
-            message.setText("İftar bilgisi öğrenmek istediğiniz şehrin adını yazıp gönderebilirsiniz. Ör:Ankara");
-        }
-        else{
+			message.setText("İftar bilgisi öğrenmek istediğiniz şehrin adını yazıp gönderebilirsiniz. Ör:Ankara");
+		} else {
 
-            System.out.println(checkCity(command));
+			System.out.println(checkCity(command));
 
-            String reply = service.askForCity(checkCity(command));
+			String reply = service.askForCity(checkCity(command));
 
-            message.setText(checkCity(reply));
+			message.setText(checkCity(reply));
 
-        }
+		}
 
-        System.out.println(update.toString());
-        message.setChatId(update.getMessage().getChatId());
+		System.out.println(update.toString());
+		message.setChatId(update.getMessage().getChatId());
 
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+		try {
+			execute(message);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 
+	}
 
-    }
+	public String checkCity(String cityName) {
+		for (String city : cities) {
 
-    public String checkCity(String cityName){
-        for(String city : cities){
+			String cityNameEn = clearTurkishChars(cityName.toUpperCase());
 
-            String cityNameEn = clearTurkishChars(cityName.toUpperCase());
+			if (cityNameEn.equals(city)) {
+				System.out.println(cityNameEn);
+				return cityNameEn;
+			}
+		}
 
-            if (cityNameEn.equals(city)){
-                System.out.println(cityNameEn);
-                return cityNameEn;
-            }
-        }
+		return cityName + " isimli şehir bulunamadı.";
+	}
 
-        return cityName + " isimli şehir bulunamadı.";
-    }
+	// https://gist.github.com/onuryilmaz/6034569
+	public static String clearTurkishChars(String str) {
+		String ret = str;
+		char[] turkishChars = new char[] { 0x131, 0x130, 0xFC, 0xDC, 0xF6, 0xD6, 0x15F, 0x15E, 0xE7, 0xC7, 0x11F,
+				0x11E };
+		char[] englishChars = new char[] { 'i', 'I', 'u', 'U', 'o', 'O', 's', 'S', 'c', 'C', 'g', 'G' };
+		for (int i = 0; i < turkishChars.length; i++) {
+			ret = ret.replaceAll(new String(new char[] { turkishChars[i] }),
+					new String(new char[] { englishChars[i] }));
+		}
+		return ret;
+	}
 
-    //https://gist.github.com/onuryilmaz/6034569
-    public static String clearTurkishChars(String str) {
-        String ret = str;
-        char[] turkishChars = new char[] {0x131, 0x130, 0xFC, 0xDC, 0xF6, 0xD6, 0x15F, 0x15E, 0xE7, 0xC7, 0x11F, 0x11E};
-        char[] englishChars = new char[] {'i', 'I', 'u', 'U', 'o', 'O', 's', 'S', 'c', 'C', 'g', 'G'};
-        for (int i = 0; i < turkishChars.length; i++) {
-            ret = ret.replaceAll(new String(new char[]{turkishChars[i]}), new String(new char[]{englishChars[i]}));
-        }
-        return ret;
-    }
+	public String getBotUsername() {
+		return "";
+	}
 
-    public String getBotUsername() {
-        return "your-bot-userName";
-    }
-
-    public String getBotToken() {
-        return "your-bot-token";
-
-    }
+	public String getBotToken() {
+		return "";
+	}
 }
-
